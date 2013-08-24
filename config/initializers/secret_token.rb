e1197763c20c1a4945ea111a4739b14115b8f1fe
Peter Li <peterjsss@gmail.com>
2013-08-24 10:38:46 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Xhero::Application.config.secret_key_base = '4f610fecbb2f56ccdc725d9dee47ede38427bb263b2261d202df544cac0921821942926dbdd2524247855e58608b2ac5c73d94e735e904466140a7014c55cd9b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Xhero::Application.config.session_store :cookie_store, key: '_xhero_session'
