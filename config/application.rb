a43a8aeb7b83fed4d3a72bb60cfa72750e847610
Peter Li <peterjsss@gmail.com>
2013-08-24 12:00:42 +0800

    debut routes

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module Xhero
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.middleware.insert_after ActionDispatch::ParamsParser, ActionDispatch::XmlParamsParser
 
     config.i18n.default_locale = :en
     config.weixin_token = 'xherodemoapp';
